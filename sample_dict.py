226f037dd93eab7a5a08dc7a2ba4c3d88663fec2
athulanish <athulanish6@gmail.com>
2021-07-14 19:50:43 +0530

    Saved

---
+++ sample_dict.py
@@ -0,0 +1,59 @@
+#DICTIONARY
+sample_dict = {
+    "AN":"Andaman and Nicobar Islands",
+    "AP":"Andhra Pradesh",
+    "AR":"Arunachal Pradesh",
+    "AS":"Assam",
+    "BR":"Bihar",
+    "CG":"Chandigarh",
+    "CH":"Chhattisgarh",
+    "DN":"Dadra and Nagar Haveli",
+    "DD":"Daman and Diu",
+    "DL":"Delhi",
+    "GA":"Goa",
+    "GJ":"Gujarat",
+    "HR":"Haryana",
+    "HP":"Himachal Pradesh",
+    "JK":"Jammu and Kashmir",
+    "JH":"Jharkhand",
+    "KA":"Karnataka",
+    "KL":"Kerala",
+    "LA":"Ladakh",
+    "LD":"Lakshadweep",
+    "MP":"Madhya Pradesh",
+    "MH":"Maharashtra",
+    "MN":"Manipur",
+    "ML":"Meghalaya",
+    "MZ":"Mizoram",
+    "NL":"Nagaland",
+    "OR":"Odisha",
+    "PY":"Puducherry",
+    "PB":"Punjab",
+    "RJ":"Rajasthan",
+    "SK":"Sikkim",
+    "TN":"Tamil Nadu",
+    "TS":"Telangana",
+    "TR":"Tripura",
+    "UP":"Uttar Pradesh",
+    "UK":"Uttarakhand",
+    "WB":"West Bengal"
+}
+print(sample_dict)
+
+print("TN" in sample_dict)
+print(sample_dict["TN"])
+
+##
+print(sample_dict.get("XN", "Country not present in India"))
+
+copy_dict = sample_dict
+print(copy_dict.get("TN"))
+
+##Change value for a key
+sample_dict["TN"] = "Madras"
+print(sample_dict["TN"])
+print(copy_dict.get("TN"))
+
+
+
+
