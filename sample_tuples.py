1d52534117cada7acb1aceef514a7b51b4733160
athulanish <athulanish6@gmail.com>
2021-07-18 13:51:40 +0530

    Saved

---
+++ sample_tuples.py
@@ -0,0 +1,31 @@
+##Lets create a tuple 
+A = 1,2,3,4
+print(A)
+
+B = (1,2,4,4,5,6)
+print(B)
+
+C = (1,)
+print(C)
+
+D = ('Name', 'Age', 'Animal', 4,5,2,1)
+print(D)
+
+##Index in tuple
+
+print(A.index(2))
+
+a,b,c,d,e,f = B
+print(a,b,c,d,e,f)
+
+##Add an item in tuple
+J = A + B
+print(J)
+
+##how to convert tuple to string
+L = str(D)
+print(type(L))
+
+
+
+
