05e0e8561a2493988af0f9d030848ba6a9c7e8ae
athulanish <athulanish6@gmail.com>
2021-07-19 09:15:05 +0530

    Saved

---
+++ ternary_operator.py
@@ -0,0 +1,19 @@
+## A ternary operator is a simpler version of an if-else statement
+
+a = 24
+b = 35 
+#get an minimum output for a if its smaller than b
+print("a is minimum" if a<b else "a is maximum")
+
+good_movie = True
+print("Movie is a blockbuster" if good_movie else "Movie is a one time watch")
+##lets try the above exercise using tuple method
+print(("Movie is a one time watch","Movie is a blockbuster")[good_movie])
+
+num = 13
+print("Even" if num%2 == 0 else "Odd")
+##using tuple method
+print(("Odd", "Even")[num%2 == 0])
+
+
+
